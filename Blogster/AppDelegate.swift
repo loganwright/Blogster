90540b4e7540419d9ffbee763e3b67ec2c17387a
Logan Wright <logan.william.wright@gmail.com>
2015-05-07 19:27:03 -0400

    initial posts list setup.  Create new controller, include nib, add nibNamed initializer, create new folder ui

--- Blogster/AppDelegate.swift
+++ Blogster/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window!.rootViewController = ViewController()
+        window!.rootViewController = PostsListController()
         window!.makeKeyAndVisible()
         return true
     }
---
+++ Blogster/UI/PostsListController.swift
@@ -0,0 +1,47 @@
+//
+//  PostsListController.swift
+//  Blogster
+//
+//  Created by Logan Wright on 5/7/15.
+//  Copyright (c) 2015 lowriDevs. All rights reserved.
+//
+
+import UIKit
+
+class PostsListController: UIViewController {
+
+    // MARK: Initialization
+    
+    init() {
+        super.init(nibName: "PostsListController", bundle: nil)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
